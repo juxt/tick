c2b1150e8127197535693b014e32763c32e8ee1b
Malcolm Sparks <malcolm@juxt.pro>
2017-01-26 16:49:02 +0000

    Release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;; Copyright © 2016, JUXT LTD.
 
-(defproject tick "0.2.0"
+(defproject tick "0.2.1"
   :description "A Clojure library that deals with time"
   :url "https://github.com/juxt/tick"
   :license {:name "The MIT License"
