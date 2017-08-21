3d7418d93f41e7d1df440d4f0010c96552c6fcb0
Malcolm Sparks <malcolm@juxt.pro>
2017-08-21 11:49:41 +0100

    Initial implementation of error-handler
    This still needs to be debugged and may not work properly in all cases.

--- src/tick/schedule.clj
+++ src/tick/schedule.clj
@@ -9,27 +9,45 @@
     (let [dly (.until (.instant clock) (:tick/date next-time) ChronoUnit/MILLIS)]
       (.schedule executor ^Callable callback dly TimeUnit/MILLISECONDS))))
 
-(defn callback [state timeline {:keys [clock trigger executor promise] :as opts}]
+(defn trigger-events [f values error-handler]
+  (loop [[v] values]
+    (when v
+      (let [r
+            (try
+              (f v)
+              :continue ; TODO: Might be a good feature to allow a trigger to influence continuation
+              (catch Exception e
+                (error-handler e)))]
+        (if (= :continue r)
+          (recur (next values))
+          :abort)))))
+
+(defn callback [state timeline {:keys [clock trigger executor promise error-handler] :as opts}]
   (let [[due next-timeline] (split-with #(not (.isAfter (.toInstant (:tick/date %)) (.instant clock))) timeline)]
 
     (if-not (empty? next-timeline)
       ;; Schedule next
       (do
-        (swap! state assoc
-               :timeline next-timeline
-               :scheduled-future (schedule-next (first next-timeline)
+        (let [fut (schedule-next (first next-timeline)
                                                 {:clock clock
                                                  :executor executor
-                                                 :callback #(callback state next-timeline opts)}))
-        (dorun (map trigger due)))
+                                                 :callback #(callback state next-timeline opts)})]
+          (swap! state assoc
+                 :timeline next-timeline
+                 :scheduled-future fut)
+          (when (= :abort (trigger-events trigger due error-handler))
+            (.cancel fut false)
+            (.shutdown executor)
+            ;; Early delivery of promise
+            (deliver promise :fail))))
 
       ;; Set status to :done
       (do (swap! state (fn [s] (-> s
                                    (assoc :status :done)
                                    (dissoc :timeline :scheduled-future))))
-          (dorun (map trigger due))
+          (trigger-events trigger due error-handler)
+          (.shutdown executor)
           (deliver promise :done)))))
-
 
 (defprotocol ITicker
   "A ticker travels across a timeline, usually triggering some action for each time on the timeline."
@@ -40,7 +58,7 @@
   (remaining [_] "Return the remaining timeline yet to be visited by the ticker.")
   (clock [_] "Return the clock indicating where the ticker is in the timeline."))
 
-(defrecord SchedulingTicker [trigger timeline executor state promise]
+(defrecord SchedulingTicker [trigger timeline executor state promise error-handler]
   ITicker
   (start [_ clock]
     (swap! state assoc
@@ -50,7 +68,12 @@
                               (first timeline)
                               {:clock clock
                                :executor executor
-                               :callback #(callback state timeline {:clock clock :trigger trigger :executor executor :promise promise})})
+                               :callback #(callback state timeline
+                                                    {:clock clock
+                                                     :trigger trigger
+                                                     :executor executor
+                                                     :promise promise
+                                                     :error-handler error-handler})})
            :clock clock
            :executor executor)
     promise)
@@ -73,7 +96,12 @@
                                                                  (first timeline)
                                                                  {:clock clock
                                                                   :executor executor
-                                                                  :callback #(callback state timeline {:clock clock :trigger trigger :executor executor :promise promise})}))))))
+                                                                  :callback #(callback state timeline
+                                                                                       {:clock clock
+                                                                                        :trigger trigger
+                                                                                        :executor executor
+                                                                                        :promise promise
+                                                                                        :error-handler error-handler})}))))))
 
         :ok)))
 
@@ -81,6 +109,7 @@
     (when-let [fut (:scheduled-future @state)]
       (.cancel fut false))
     (swap! state (fn [s] (-> s (assoc :status :stopped) (dissoc :scheduled-future))))
+    (.shutdown executor)
     :ok)
 
   (remaining [_]
@@ -88,19 +117,25 @@
   (clock [_]
     (:clock @state)))
 
+(defn default-error-handler [err]
+  (println "Error" err)
+  (.printStackTrace err)
+  :continue)
+
 (defn schedule
   "Think of this like map, but applying a function over a timeline. Returns a ticker."
   ([trigger timeline]
    (schedule trigger timeline {}))
-  ([trigger timeline {:keys [executor]}]
+  ([trigger timeline {:keys [executor error-handler]}]
    (map->SchedulingTicker
     {:trigger trigger
      :timeline timeline
      :state (atom {})
      :executor (or executor (new ScheduledThreadPoolExecutor 16))
-     :promise (promise)})))
+     :promise (promise)
+     :error-handler (or error-handler default-error-handler)})))
 
-(defrecord ImpatientTicker [trigger timeline state executor]
+(defrecord ImpatientTicker [trigger timeline state executor error-handler]
   ITicker
   (start [this clock]
     (swap! state assoc
@@ -119,13 +154,16 @@
                     :clock (Clock/fixed (.toInstant (:tick/date tick)) (.getZone clock))
                     :timeline (next timeline))
              ;; Call trigger
-             (trigger tick)
-             (recur (next timeline))))))))
+             (when-not (= :abort (trigger-events trigger [tick] error-handler))
+               (recur (next timeline))
+               ;; TODO: Early delivery of promise?
+               )))))))
 
   (pause [this] :unsupported)
   (resume [this] :unsupported)
   (stop [this]
     (swap! state assoc :status :stopped)
+    (.shutdown executor)
     :ok)
   (remaining [this] (:timeline @state))
   (clock [this] (:clock @state)))
@@ -135,8 +173,9 @@
   to the next time in the timeline and serially executes the trigger."
   ([trigger timeline]
    (simulate trigger timeline {}))
-  ([trigger timeline {:keys [executor]}]
+  ([trigger timeline {:keys [executor error-handler]}]
    (map->ImpatientTicker {:trigger trigger
                           :timeline timeline
                           :state (atom {})
-                          :executor (or executor (new ScheduledThreadPoolExecutor 1))})))
+                          :executor (or executor (new ScheduledThreadPoolExecutor 1))
+                          :error-handler (or error-handler default-error-handler)})))
--- test/tick/schedule_test.clj
+++ test/tick/schedule_test.clj
@@ -25,3 +25,34 @@
         timeline (take 1000 (timeline (periodic-seq now (seconds 1))))]
     @(sched/start (sched/simulate f timeline) clk)
     (is (= @a 1000))))
+
+(deftest schedule-with-error-handler-test
+  (let [a (atom 0)
+        f (fn [dt] (swap! a inc))
+        clk (clock-ticking-in-seconds)
+        now (just-now clk)
+        timeline (take 10 (timeline (periodic-seq now (millis 10))))]
+    @(sched/start (sched/schedule f timeline) clk)
+    (is (= @a 10))))
+
+
+#_(let [a (atom 0)
+      f (fn [dt h] (swap! a inc))
+      clk (clock-ticking-in-seconds)
+      now (just-now clk)
+      timeline (take 4 (timeline (periodic-seq now (millis 100))))
+      eh (fn [err] (println "error!" err) :continue)]
+  @(sched/start (sched/schedule f timeline {:error-handler eh}) clk)
+  (is (= @a 0))
+  )
+
+
+(deftest simulate-with-error-handler-test
+  (let [a (atom 0)
+        f (fn [dt] (swap! a inc) (throw (ex-info "error" {})))
+        clk (clock-ticking-in-seconds)
+        now (just-now clk)
+        timeline (take 4 (timeline (periodic-seq now (seconds 1))))
+        eh (fn [err] :continue)]
+    @(sched/start (sched/simulate f timeline {:error-handler eh}) clk)
+    (is (= @a 4))))
