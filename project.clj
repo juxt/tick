66bb5df0f59a17203d8a1abeaf4162e2bfae1f91
Malcolm Sparks <malcolm@juxt.pro>
2017-12-02 14:36:54 +0000

    Relegate graphics dependencies to REPL only

--- project.clj
+++ project.clj
@@ -7,8 +7,10 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/spec.alpha "0.1.94"]]
   :profiles {:dev
-             {:dependencies [[org.clojure/clojure "1.9.0-beta3"]
-                             [org.apache.xmlgraphics/batik-swing "1.9"]
-                             [net.mikera/core.matrix "0.61.0"]]
+             {:dependencies [[org.clojure/clojure "1.9.0-beta3"]]
               :jvm-opts ["-Dclojure.spec.compile-asserts=true"]}
-             :repl {:source-paths ["dev/src"]}})
+             :repl {:dependencies
+                    [[org.clojure/data.xml "0.2.0-alpha5"]
+                     [net.mikera/core.matrix "0.61.0"]
+                     [org.apache.xmlgraphics/batik-swing "1.9"]]
+                    :source-paths ["dev/src"]}})
