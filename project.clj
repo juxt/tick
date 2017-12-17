b6def7c11f817215a59fc4685e14074445abbf32
Malcolm Sparks <malcolm@juxt.pro>
2017-12-17 09:55:34 +0000

    Upgrade to Clojure 1.9.0

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 ;; Copyright © 2016, JUXT LTD.
 
-(defproject tick "0.3.6-SNAPSHOT"
+(defproject tick "0.4.0-SNAPSHOT"
   :description "A Clojure library that deals with time"
   :url "https://github.com/juxt/tick"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/spec.alpha "0.1.94"]]
+  :dependencies []
   :profiles {:dev
-             {:dependencies [[org.clojure/clojure "1.9.0-beta3"]]
+             {:dependencies [[org.clojure/clojure "1.9.0"]]
               :jvm-opts ["-Dclojure.spec.compile-asserts=true"]}
-             :repl {:dependencies
-                    [[org.clojure/data.xml "0.2.0-alpha5"]
-                     [net.mikera/core.matrix "0.61.0"]
-                     [org.apache.xmlgraphics/batik-swing "1.9"]]
-                    :source-paths ["dev/src"]}})
+             :repl
+             {:dependencies
+              [[org.clojure/data.xml "0.2.0-alpha5"]
+               [org.apache.xmlgraphics/batik-swing "1.9"]]
+              :source-paths ["dev/src"]}})
