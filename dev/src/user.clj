eb31c862b5f4dbc776acf889a09d9b5f7bec4b55
Malcolm Sparks <malcolm@juxt.pro>
2018-03-05 18:08:23 +0000

    Fix to warnings on repl

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -1,7 +1,6 @@
 (ns user
-  (:refer-clojure :exclude [dec < range <= min long int > - time / >= inc + max complement atom])
   (:require
-   [tick.alpha.api :refer :all]
+   [tick.alpha.api :as t]
    ;;[tick.deprecated.cal :refer [holidays-in-england-and-wales weekend?]]
    [tick.viz :refer [show-canvas view label]]
    ;;[tick.deprecated.schedule :as sch :refer [schedule]]
