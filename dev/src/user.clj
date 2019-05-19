6632ef132dd59f6d367c2da050cd0b1abd2b1606
Henry Widd <henryw374@gmail.com>
2019-05-19 08:41:05 +0100

    add dev tools

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -4,21 +4,25 @@
    [tick.viz :refer [show-canvas view label]]
    [clojure.spec.alpha :as s]
    [cljs.cljs-repl :as cljs-repl]
+   [clojure.tools.namespace.repl :refer [refresh refresh-all]]
    [infra]
    clojure.test)
   (:import [java.time DayOfWeek]))
+
+(set! *warn-on-reflection* true)
 
 (when (System/getProperty "nrepl.load")
   (require 'nrepl))
 
 (defn test-all []
-  (require
-    'tick.alpha.api-test
-    'tick.alpha.api.dates-test
-    'tick.core-test
-    'tick.interval-test
-    'tick.ical-test)
+  (refresh)
   (clojure.test/run-all-tests #"(tick).*test$"))
 
 (defn node []
   (cljs-repl/node-repl))
+
+(comment 
+  (refresh-all)
+  (test-all)
+  
+  )
