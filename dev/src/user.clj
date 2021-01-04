470eb3aaced88a37ce673793f5122f4ca5a66489
Andrea Crotti <andrea.crotti.0@gmail.com>
2021-01-04 14:34:03 +0000

    use kaocha also in test-all

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -1,23 +1,22 @@
 (ns user
   (:require
-   [tick.alpha.api :as t]
-   [tick.viz :refer [show-canvas view label]]
-   [clojure.spec.alpha :as s]
-   [clojure.tools.namespace.repl :refer [refresh refresh-all]]
    [cljs :refer :all]
-   clojure.test))
+   [clojure.tools.namespace.repl :refer [refresh refresh-all]]
+   [kaocha.repl :as kr]
+   [tick.alpha.api :as t]
+   [tick.viz :refer [show-canvas view label]]))
 
 (set! *warn-on-reflection* true)
 
 (when (System/getProperty "nrepl.load")
   (require 'nrepl))
 
-(defn test-all []
-  (refresh)
-  (clojure.test/run-all-tests #"(tick).*test$"))
+(defn test-clj* [] (kr/run :clj))
 
-(comment 
+(defn test-clj []
+  (refresh :after 'user/test-clj))
+
+(comment
   (refresh-all)
-  (test-all)
-  
-  )
+  (test-clj)
+)
