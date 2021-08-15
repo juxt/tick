09c01ad31285ea5ebfc0002b6dc662cf00eb1db0
Henry Widd <henryw374@gmail.com>
2021-08-15 06:51:37 +0100

    warn that converting to and from Instants to other types requires care if execution env is not fixed to UTC

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -1,12 +1,21 @@
 (ns user
   (:require
-   [cljs :refer :all]
-   [clojure.tools.namespace.repl :refer [refresh refresh-all]]
-   [kaocha.repl :as kr]
-   [tick.core :as t]
-   [tick.viz :refer [show-canvas view label]]))
+    [cljs :refer :all]
+    [clojure.tools.namespace.repl :refer [refresh refresh-all]]
+    [kaocha.repl :as kr]
+    [tick.core :as t]
+    [tick.viz :refer [show-canvas view label]])
+  (:import (java.util TimeZone)))
 
 (set! *warn-on-reflection* true)
+
+(defn set-zone [tz]
+  (TimeZone/setDefault (TimeZone/getTimeZone tz))
+  (alter-var-root #'t/*clock*
+    (constantly (cljc.java-time.zone-id/system-default)))
+  )
+
+(set-zone "America/New_York")
 
 (when (System/getProperty "nrepl.load")
   (require 'nrepl))
